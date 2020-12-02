28c91631853875df0051a998e1248095f06d3b0c
Miguel <parrale93@gmail.com>
2020-12-02 20:21:32 +0100

    otro commit

--- app/src/main/java/com/example/mynicestart/MainActivity.java
+++ app/src/main/java/com/example/mynicestart/MainActivity.java
@@ -3,9 +3,7 @@ package com.example.mynicestart;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
